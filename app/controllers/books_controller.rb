6b719bddad33946caeb7d8a183bf9f29897fd01e
yutomorita <yutomorita416@gmail.com>
2021-05-08 12:15:00 +0000

    update index & create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,25 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
   end
-
-  def new
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def edit
   end
+  
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
