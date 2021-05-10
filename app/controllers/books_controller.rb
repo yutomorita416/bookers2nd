b5f4c6cb2780787827eaffd08afd3f66acbe9e37
yutomorita <yutomorita416@gmail.com>
2021-05-10 01:22:44 +0000

    add flash message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,8 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(book.id)
   end
 
   def edit
@@ -21,12 +22,14 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book)
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
   
